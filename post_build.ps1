d5295fbffc06bf50e7efe4f2236608dbf746077a
Matthew Fraser <1923697+blendermf@users.noreply.github.com>
2019-01-06 00:00:33 -0500

    Finished build events for all projects that I thought I already did ages ago. Oops. Changed post build to now automatically create zip files of the mods, automatically pulling out the version number from Info.json to put in the filename (release builds only). Fixed a bug with post build that prevented XLShredLib from copying for the Menu Mod.

--- post_build.ps1
+++ post_build.ps1
@@ -4,6 +4,7 @@ param(
     [String]$TargetDir = $(throw "-TargetDir is required."),
     [String]$ProjectDir = $(throw "-ProjectDir is required."),
     [String]$SolutionDir = $(throw "-SolutionDir is required."),
+    [String]$ConfigurationName = $(throw "-ConfigurationName is required."),
     [Switch]$LoadLib
 )
 
@@ -14,9 +15,18 @@ $gameDirectory = $configContent.game_directory;
 New-Item -ItemType directory -Path ([io.path]::combine( $gameDirectory, "Mods\", $TargetName + "\" )) -Force;
 
 copy-item $TargetPath ([io.path]::combine( $gameDirectory, "Mods\", $TargetName)) -force;
-copy-item ([io.path]::combine( $TargetDir, $TargetName + ".pdb" )) ([io.path]::combine( $gameDirectory, "Mods\", $TargetName )) -force;
+if ($ConfigurationName -eq "Debug") {
+    copy-item ([io.path]::combine( $TargetDir, $TargetName + ".pdb" )) ([io.path]::combine( $gameDirectory, "Mods\", $TargetName )) -force;
+}
 copy-item ([io.path]::combine( $ProjectDir, "Resources\Info.json" )) ([io.path]::combine( $gameDirectory, "Mods\", $TargetName )) -force;
 If ($LoadLib) {
-    copy-item ([io.path]::combine( $TargetDir, "XLShredLib.dll" )) ([io.path]::combine( $gameDirectory, "Mods\", $TargetName )) -force;
-    copy-item ([io.path]::combine( $TargetDir, "XLShredLib.pdb" )) ([io.path]::combine( $gameDirectory, "Mods\", $TargetName )) -force;
+    copy-item ([io.path]::combine( $TargetDir, "XLShredLib.dll" )) ([io.path]::combine( $gameDirectory, "Mods\", $TargetName ))  -force;
+    if ($ConfigurationName -eq "Debug") {
+        copy-item ([io.path]::combine( $TargetDir, "XLShredLib.pdb" )) ([io.path]::combine( $gameDirectory, "Mods\", $TargetName )) -force;
+    }
+}
+
+if ($ConfigurationName -eq "Release") {
+    $infoContent = Get-Content ([io.path]::combine($ProjectDir, 'Resources\Info.json')) | ConvertFrom-Json;
+    Compress-Archive -Update -Path ([io.path]::combine( $gameDirectory, "Mods\", $TargetName )) -DestinationPath ([io.path]::combine( $gameDirectory, "Mods\", $TargetName + '-' + $infoContent.version + '.zip' ));
 }
