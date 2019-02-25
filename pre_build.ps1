3286accab071caa3d6b234611bc3bcf5b3d109ad
Daniel Ivert <ivert.daniel@gmail.com>
2019-02-26 00:02:32 +0100

    Updated projects to pull the correct references for Unity Mod Manager 0.13.0.0

--- pre_build.ps1
+++ pre_build.ps1
@@ -6,4 +6,5 @@ $configContent = Get-Content ([io.path]::combine($SolutionDir, "config.json")) |
 
 $gameDirectory = $configContent.game_directory;
 
-robocopy ([io.path]::combine( $gameDirectory, "SkaterXL_Data\Managed")) ([io.path]::combine($SolutionDir , "references")) "Assembly-CSharp.dll" "Assembly-CSharp-firstpass.dll" "0Harmony12.dll" "UnityEngine.dll" "UnityEngine.UI.dll" "UnityEngine.CoreModule.dll" "UnityEngine.IMGUIModule.dll" "UnityEngine.PhysicsModule.dll" "UnityEngine.AnimationModule.dll" "Rewired_Core.dll" "UnityEngine.AssetBundleModule.dll"/COPY:DAT /XF
+robocopy ([io.path]::combine( $gameDirectory, "SkaterXL_Data\Managed")) ([io.path]::combine($SolutionDir , "references")) "Assembly-CSharp.dll" "Assembly-CSharp-firstpass.dll" "UnityEngine.dll" "UnityEngine.UI.dll" "UnityEngine.CoreModule.dll" "UnityEngine.IMGUIModule.dll" "UnityEngine.PhysicsModule.dll" "UnityEngine.AnimationModule.dll" "Rewired_Core.dll" "UnityEngine.AssetBundleModule.dll"/COPY:DAT /XF
+robocopy ([io.path]::combine( $gameDirectory, "SkaterXL_Data\Managed\UnityModManager")) ([io.path]::combine($SolutionDir , "references")) "0Harmony12.dll" "UnityModManager.dll" /COPY:DAT /XF
