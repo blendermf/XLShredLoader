b1504a99e391d0bdbd9ad3b43c445fe431d71d54
Matthew Fraser <1923697+blendermf@users.noreply.github.com>
2019-03-22 04:36:05 -0400

    Fixed Realistic Vert Made it behave a lot better in general (generally you shoot up from vert, don't overshoot, and don't push away too much). Resolves #4 Removed debug display from Realistic Vert.

--- XLShredRealisticVert/Patches/PlayerControllerPatches.cs
+++ XLShredRealisticVert/Patches/PlayerControllerPatches.cs
@@ -29,89 +29,33 @@ namespace XLShredRealisticVert.Patches {
 
         static void Postfix(PlayerController __instance, float p_pop, float p_scoop) {
             if (Main.settings.realisticVert && Main.enabled) {
-                Vector3 popDir = new Vector3(0f, 1f, 0f) * p_pop;
+                Vector3 popDir = Vector3.up * p_pop;
                 Rigidbody skaterBody = __instance.skaterController.skaterRigidbody;
                 Vector3 p_up = __instance.boardController.GroundNormal;
-
+                Vector3 forwardNoY = new Vector3(-__instance.GetGroundNormal().x, 0, -__instance.GetGroundNormal().z).normalized;
                 if (Vector3.Angle(__instance.GetGroundNormal(), Vector3.up) > 26.0f) {
-                    Vector3 forwardNoY = new Vector3(-__instance.GetGroundNormal().x, 0, -__instance.GetGroundNormal().z).normalized;
                     float z = Vector3.Project(skaterBody.velocity, forwardNoY).magnitude * __instance.skaterController.skaterRigidbody.mass;
-                   // float y = z / Mathf.Tan(Mathf.Asin(z / p_pop));
-                    popDir = (-forwardNoY.normalized * Mathf.Max(z - 0.5f, 0f)) + (Vector3.up * p_pop);
-                        //popDir.x = (Mathf.Abs(popDir.x) >= Mathf.Abs(forwardNoY.x * z)) ? popDir.x : -forwardNoY.x * z;
-                        //popDir.z = (Mathf.Abs(popDir.z) >= Mathf.Abs(forwardNoY.z * z)) ? popDir.x : -forwardNoY.z * z;
-                    
+                    float angle_percent = Vector3.Angle(__instance.GetGroundNormal(), Vector3.up) / 80f;
+                    popDir = (-forwardNoY.normalized * Mathf.Max(z - (0.4f * (1.0f - angle_percent)), 0f) * angle_percent) + (Vector3.up * p_pop);
                 }
-                Console.WriteLine(Vector3.Angle(__instance.GetGroundNormal(), Vector3.up));
-                __instance.skaterController.skaterRigidbody.AddForce(popDir, ForceMode.Impulse);
+                Vector3 vector = popDir;
+                Vector3 to = __instance.skaterController.skaterRigidbody.velocity + vector;
+                Vector3.Angle(__instance.cameraController._actualCam.forward, to);
+                Vector3 force = __instance.skaterController.PredictLanding(vector);
+                __instance.skaterController.skaterRigidbody.AddForce(vector, ForceMode.Impulse);
+                __instance.skaterController.skaterRigidbody.AddForce(force, ForceMode.VelocityChange);
                 SoundManager.Instance.PlayPopSound(p_scoop);
-                __instance.comController.popForce = popDir;
-
-
-
+                __instance.comController.popForce = vector;
             } else {
-                __instance.skaterController.skaterRigidbody.AddForce(__instance.skaterController.skaterTransform.up * p_pop, ForceMode.Impulse);
+                Vector3 vector = __instance.skaterController.skaterTransform.up * p_pop;
+                Vector3 to = __instance.skaterController.skaterRigidbody.velocity + vector;
+                Vector3.Angle(__instance.cameraController._actualCam.forward, to);
+                Vector3 force = __instance.skaterController.PredictLanding(vector);
+                __instance.skaterController.skaterRigidbody.AddForce(vector, ForceMode.Impulse);
+                __instance.skaterController.skaterRigidbody.AddForce(force, ForceMode.VelocityChange);
                 SoundManager.Instance.PlayPopSound(p_scoop);
-                __instance.comController.popForce = __instance.skaterController.skaterTransform.up * p_pop;
+                __instance.comController.popForce = vector;
             }
         }
-    }
-
-    [HarmonyPatch(typeof(PlayerController), "OnPop")]
-    [HarmonyPatch(new [] { typeof(float), typeof(float), typeof(Vector3) })]
-    static class PlayerController_Pop_PopDir_Patch {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
-            var codes = instructions.ToList();
-
-            for (int i = 0; i < codes.Count; i++) {
-                var inst = codes[i];
-
-                if (inst.opcode == OpCodes.Call
-                    && (MethodInfo)inst.operand == AccessTools.Method(typeof(PlayerController), "SetSkaterToMaster")) {
-
-                    yield return inst;
-                    break;
-                }
-
-                yield return inst;
-            }
-        }
-
-        static void Postfix(PlayerController __instance, float p_pop, float p_scoop, Vector3 p_popOutDir) {
-            if (Main.settings.realisticVert && Main.enabled) {
-                __instance.skaterController.skaterRigidbody.AddForce((new Vector3(0f, 1f, 0f) + p_popOutDir) * p_pop, ForceMode.Impulse);
-                SoundManager.Instance.PlayPopSound(p_scoop);
-                __instance.comController.popForce = (new Vector3(0f, 1f, 0f) + p_popOutDir) * p_pop;
-            } else {
-                __instance.skaterController.skaterRigidbody.AddForce((__instance.skaterController.skaterTransform.up + p_popOutDir) * p_pop, ForceMode.Impulse);
-                SoundManager.Instance.PlayPopSound(p_scoop);
-                __instance.comController.popForce = (__instance.skaterController.skaterTransform.up + p_popOutDir) * p_pop;
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(SkaterController), "InAirRotationLogic")]
-    static class SkaterController_InAirRotationLogic_Patch {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
-            yield return new CodeInstruction(OpCodes.Nop);
-        }
-
-        static void Prefix(SkaterController __instance, bool ____landingPrediction, ref float ____startTime, ref float ____duration,
-            ref Quaternion ____startRotation, ref Vector3 ____startUpVector, ref Quaternion ____newUp) {
-            if (!____landingPrediction) {
-                ____startTime = Time.time;
-                ____duration = PlayerController.Instance.boardController.trajectory.CalculateTrajectory(__instance.skaterTransform.position - Vector3.up * 0.9765f, __instance.skaterRigidbody, 50f);
-                ____startRotation = __instance.skaterRigidbody.rotation;
-                ____startUpVector = __instance.skaterTransform.up;
-                ____landingPrediction = true;
-                //____newUp = Quaternion.FromToRotation(____startUpVector, Vector3.up);
-                ____newUp = Quaternion.identity;
-                ____newUp *= __instance.skaterRigidbody.rotation;
-                ((MonoBehaviour) __instance).Invoke("PreLandingEvent", ____duration - 0.3f);
-            }
-
-            AccessTools.Method(typeof(SkaterController), "InAirRotation").Invoke(__instance, new object[] { Mathf.Clamp((Time.time - ____startTime) / ____duration, 0f, 1f) });
-        }
-
     }
 }
